b4d6bc827152a8245a3851ade2e922d1ee44400c
Jimmyzaurio <wizard.jsp@gmail.com>
2015-11-01 22:15:38 -0600

    Create mult.c

---
+++ mult.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+#define TRES 1000
+#define SEIS 1000000
+#define NUEVE 1000000000
+#define DOCE 1000000000000
+
+#define LA a/SEIS
+#define MA (a/TRES) % TRES
+#define RA a%TRES
+
+#define LB b/SEIS
+#define MB (b/TRES) % TRES
+#define RB b%TRES
+
+int mult2(int a, int b) {
+	if(b/10 == 0 || a/10 == 0)
+		return a*b;
+	return
+		mult2(floor(a/2),  ceil(b/2)) +
+		mult2( ceil(a/2), floor(b/2)) +
+		mult2(floor(a/2), floor(b/2)) +
+		mult2( ceil(a/2),  ceil(b/2));
+}
+
+int mult3(int a, int b) {
+	if(a < 1000 && b < 1000)
+		return a*b;
+	return
+		(DOCE*mult3(LA,LB)) + (NUEVE*mult3(MA,LB)) + (SEIS*mult3(RA,LB)) +
+		(NUEVE*mult3(LA,MB))+ (SEIS*mult3(MA,MB)) + (TRES*mult3(RA,MB)) +
+		(SEIS*mult3(LA,RB)) + (TRES*mult3(MA,RB)) + (mult3(RA,RB));
+}
+
+int main() {
+	int a,b;
+
+	while(scanf("%d%d", &a, &b) == 2)
+		printf("%d\n", mult3(a,b)); 
+	return 0;
+}
